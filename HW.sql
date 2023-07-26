1e9689db851b3307a29c49c018b355754b27fb1b
Константин Пигулевский <pigulevskii2000@gmail.com>
2023-07-26 23:30:45 +0300

    Reworked

--- HW.sql
+++ HW.sql
@@ -13,7 +13,7 @@ release_date date
 create table if not exists track (
 track_id SERIAL primary key,
 name VARCHAR(100),
-duration NUMERIC not null,
+duration TIME not null,
 album_belonging INTEGER references album(album_id)
 );
 
@@ -63,30 +63,30 @@ values('Darkest Dungeon OST', '2020-05-29'), ('Hollow Knight OST', '2017-02-10')
 ('Blasphemous OST', '2019-09-10'), ('Katawa Shoujo: Enigmatic Box of Sound', '2012-01-20');
 
 insert into track(name, duration, album_belonging)
-values('The Hamlet', '2.58', '1');
+values('The Hamlet', '00:02:58', '1');
 
 insert into track(name, duration, album_belonging)
-values('A Brief Respite', '2.18', '1'), ('Return to the Warrens', '1.22', '1'),
-('The Final Combat', '5.21', '1'), ('Town in Chaos', '6.08', '1'),
-('A Mocking Thirst', '0.56', '1'), ('All Things Must Come', '3.48', '1');
+values('A Brief Respite', '00:02:18', '1'), ('Return to the Warrens', '00:01:22', '1'),
+('The Final Combat', '00:05:21', '1'), ('Town in Chaos', '00:06:08', '1'),
+('A Mocking Thirst', '00:00:56', '1'), ('All Things Must Come', '00:03:48', '1');
 
 insert into track(name, duration, album_belonging)
-values('Dirtmouth', '1.55', '2'), ('Greenpath', '3.37', '2'),
-('Mantis Lords', '1.45', '2'), ('City of Tears', '2.58', '2'),
-('Soul Sanctum', '4.30', '2'), ('Queen''s Gardens', '1.46', '2'),
-('Radiance', '2.17', '2');
+values('Dirtmouth', '00:01:55', '2'), ('Greenpath', '00:03:37', '2'),
+('Mantis Lords', '00:01:45', '2'), ('City of Tears', '00:02:58', '2'),
+('Soul Sanctum', '00:04:30', '2'), ('Queen''s Gardens', '00:01:46', '2'),
+('Radiance', '00:02:17', '2');
 
 insert into track(name, duration, album_belonging)
-values('Entregarás tu rostro a la señora', '3.25', '3'),
-('Cantes de confesión', '5.35', '3'), ('Arpegios en ocre', '3.35', '3'),
-('Para un mártir del compás', '5.30', '3'),
-('Al compás de tus contornos', '4.48', '3'),
-('Y yo fuego te daré', '3.42', '3'),
-('Su beso de plata', '3.13', '3');
+values('Entregarás tu rostro a la señora', '00:03:25', '3'),
+('Cantes de confesión', '00:05:35', '3'), ('Arpegios en ocre', '00:03:35', '3'),
+('Para un mártir del compás', '00:05:30', '3'),
+('Al compás de tus contornos', '00:04:48', '3'),
+('Y yo fuego te daré', '00:03:42', '3'),
+('Su beso de plata', '00:03:13', '3');
 
 insert into track(name, duration, album_belonging)
-values('Cold Iron', '3.03', '4'), ('Ease', '2.38', '4'), ('Hokabi', '2.21', '4'),
-('Innocence', '2.59', '4'), ('Letting My Heart Speak', '2.37', '4');
+values('Cold Iron', '00:03:03', '4'), ('Ease', '00:02:38', '4'), ('Hokabi', '00:02:21', '4'),
+('Innocence', '00:02:59', '4'), ('Letting My Heart Speak', '00:02:37', '4');
 
 insert into compilation(name, release_date)
 values('Ambience', '2020-06-14'), ('Combat', '2020-09-06'),
@@ -108,98 +108,52 @@ values('1', '1'), ('1', '2'), ('1', '8'), ('1', '9'), ('1', '11'), ('1', '12'),
 ('2', '5'), ('2', '6'), ('2', '7'), ('3', '4'), ('3', '10'), ('3', '12'), ('3', '14'),
 ('3', '20'), ('3', '21'), ('4', '7'), ('4', '10'), ('4', '19'), ('4', '25');
 
-select name, duration from track order by duration desc limit 1
+select name, duration from track order by duration desc limit 1;
 
-select name from track where duration > 3.5
+select name from track where duration >= '00:03:30';
 
-select name from compilation where release_date between '2018-01-01' and '2020-12-31'
+select name from compilation where release_date between '2018-01-01' and '2020-12-31';
 
-select name from artist where (select split_part(name, ' ', 2)) = ''
+select name from artist where (select split_part(name, ' ', 2)) = '';
 
-select name from track where name like '%My%'
+select name from track where name ilike 'My %' or name ilike '% My'
+ or name ilike '% My %' or name ilike 'My';
 
-SELECT * FROM artist_genre
-  inner JOIN artist 
-    ON artist_genre.artist_id = artist.artist_id
-  inner JOIN genre
-    ON artist_genre.genre_id = genre.genre_id where genre.genre_id = 1
-
-SELECT * FROM artist_genre
-  inner JOIN artist 
-    ON artist_genre.artist_id = artist.artist_id
-  inner JOIN genre
-    ON artist_genre.genre_id = genre.genre_id where genre.genre_id = 2
-
-SELECT * FROM artist_genre
-  inner JOIN artist 
-    ON artist_genre.artist_id = artist.artist_id
-  inner JOIN genre
-    ON artist_genre.genre_id = genre.genre_id where genre.genre_id = 3
-
-SELECT * FROM artist_genre
-  inner JOIN artist 
-    ON artist_genre.artist_id = artist.artist_id
-  inner JOIN genre
-    ON artist_genre.genre_id = genre.genre_id where genre.genre_id = 4
-
-SELECT * FROM artist_genre
-  inner JOIN artist 
-    ON artist_genre.artist_id = artist.artist_id
-  inner JOIN genre
-    ON artist_genre.genre_id = genre.genre_id where genre.genre_id = 5
+SELECT genre.name, COUNT(artist_id) FROM genre
+  left JOIN artist_genre  
+    ON artist_genre.genre_id = genre.genre_id
+   group by genre.genre_id order by genre.genre_id;
     
-    SELECT * FROM artist_genre
-  inner JOIN artist 
-    ON artist_genre.artist_id = artist.artist_id
-  inner JOIN genre
-    ON artist_genre.genre_id = genre.genre_id where genre.genre_id = 6
     
-SELECT * FROM artist_genre
-  inner JOIN artist 
-    ON artist_genre.artist_id = artist.artist_id
-  inner JOIN genre
-    ON artist_genre.genre_id = genre.genre_id where genre.genre_id = 7
-    
-SELECT * FROM artist_genre
-  inner JOIN artist 
-    ON artist_genre.artist_id = artist.artist_id
-  inner JOIN genre
-    ON artist_genre.genre_id = genre.genre_id where genre.genre_id = 8
-    
-SELECT track.name, track.album_belonging FROM track
+SELECT COUNT(track.track_id) FROM track
   JOIN album ON track.album_belonging = album.album_id where album.release_date
-  between '2019-01-01' and '2020-12-31'
+  between '2019-01-01' and '2020-12-31';
 
-SELECT avg(track.duration) FROM track
-  JOIN album ON track.album_belonging = album.album_id where album.album_id = 1
-    
-SELECT avg(track.duration) FROM track
-  JOIN album ON track.album_belonging = album.album_id where album.album_id = 2
-  
-SELECT avg(track.duration) FROM track
-  JOIN album ON track.album_belonging = album.album_id where album.album_id = 3
-  
-SELECT avg(track.duration) FROM track
-  JOIN album ON track.album_belonging = album.album_id where album.album_id = 4
-
-SELECT artist.name FROM album_artist
-  inner JOIN album 
-    ON album_artist.album_id = album.album_id
-  inner JOIN artist
-    ON album_artist.artist_id = artist.artist_id where album.release_date not between '2020-01-01' and '2020-12-31'
-    
-SELECT compilation.name FROM compilation_track, track, album_artist
-  inner JOIN compilation
-    ON compilation_track.compilation_id = compilation.compilation_id
-  inner JOIN track
-    ON compilation_track.track_id = track.track_id
+SELECT album.name, avg(track.duration) FROM track
   JOIN album ON track.album_belonging = album.album_id
-  inner JOIN album
+  group by album.album_id order by album.album_id;
+    
+
+SELECT artist.name FROM artist
+  where artist.name not IN(
+  select artist.name from artist 
+  join album_artist on album_artist.artist_id = artist.artist_id
+  join album on album_artist.album_id = album.album_id
+  where album.release_date between '2020-01-01' and '2020-12-31');
+  
+    
+SELECT compilation.name FROM compilation
+  JOIN compilation_track
+    ON compilation_track.compilation_id = compilation.compilation_id
+  JOIN track
+    ON compilation_track.track_id = track.track_id
+  JOIN album 
+    ON track.album_belonging = album.album_id
+  JOIN album_artist
     ON album_artist.album_id = album.album_id
-  inner JOIN artist
-    ON album_artist.artist_id = artist.artist_id where artist.artist_id = 1
-    
-    
+  JOIN artist
+    ON album_artist.artist_id = artist.artist_id
+   where artist.name = 'Carlos Viola'; 
     
     
     
